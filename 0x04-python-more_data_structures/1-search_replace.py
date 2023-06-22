7d46e2d953e23fec80057953db5225d54af36606
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-06-22 04:54:26 -0700

    task 1 update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n_list = []
-    for element in my_list:
-        if element == search:
-            n_list.append(replace)
-        else:
-            n_list.append(element)
-    return n_list
+    new_list = []
+    for i in my_list:
+        new_list.append(i if i != search else replace)
+    return new_list
