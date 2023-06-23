f96a170e9829fcf3556316dfdde7dbc20b0d3387
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-06-22 19:23:27 -0700

    task 10 update 4

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
         if value == max(a_dictionary.values()):
