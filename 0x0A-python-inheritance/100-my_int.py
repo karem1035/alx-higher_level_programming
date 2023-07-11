c1482e6e64c6ab3f0662e578ea04dfc00c99cd1a
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-10 19:13:37 -0700

    task fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
-"""
-Module with a class Myint
-"""
+"""MyInt class module"""
 
 
 class MyInt(int):
-    """class that inherits from int"""
-
-    def __init__(self, my_int):
-        """for initialize a value my_int"""
-
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
-
+    """A MyInt class"""
     def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
+        """Overides and inverts == operator"""
+        return int(self) != int(other)
 
     def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+        """Overides and inverts != operator"""
+        return int(self) == int(other)
