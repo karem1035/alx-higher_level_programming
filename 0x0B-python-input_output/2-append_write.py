68ee61931cb6e2fdad2e06539fd080c1732997ed
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-11 17:55:26 -0700

    fixes

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function that appends a string"""
 
+
 def append_write(filename="", text=""):
     """Function that appends a string at the end
     of a text file (UTF8) and returns the number
