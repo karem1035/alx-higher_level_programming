ebcc71ee7f028d800c6f244796158584c6e8e2ec
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2023-07-18 10:18:40 +0000

    return value fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,4 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
-        print("{} is not an integer".format(str(value)))
         return False
