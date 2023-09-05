208165a7ebd163e91ca3e25cffe0a34a07851fef
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-05 22:04:41 +0300

    added some comments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,8 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-
+    
+    """sett the height"""
     @height.setter
     def height(self, value):
         if type(value) != int:
@@ -25,7 +26,8 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-
+    
+    """width setter"""
     @width.setter
     def width(self, value):
         if type(value) != int:
