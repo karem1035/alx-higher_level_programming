740347cd74b062d02cf22134580753a69ebbaaf8
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-12 02:44:01 +0300

    task 0 updated

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,2 +1,11 @@
 def lookup(obj):
+    """A function that taks a Calss and returns its components
+
+    Args:
+        obj : The object we are going to use
+
+    Returns:
+        str : returns the list of available attributes and methods of an object
+    """
+
     return (dir(obj))
