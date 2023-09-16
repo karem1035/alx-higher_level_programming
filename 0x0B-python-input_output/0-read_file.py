0d7fa2283f77ec85088ecb4b0f9534871bade1a3
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-16 03:48:17 +0300

    task 0 python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,8 @@
 
 
 def read_file(filename=""):
+    """Opens a file and prints its content"""
+
     with open(filename, encoding="UTF8") as a_file:
         for a_line in a_file:
             print(a_line, end="")
