4d7eaa8be32d39e9c546c4f34b5982f424d81efd
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-17 07:24:38 +0300

    small updated task 7

--- 7-add_item.py
+++ 7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list, and then save them to a file:
+"""Adds all arguments to a Python list, and then save them to a file:
 """
 import sys
 
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        "6-load_from_json_file").load_from_json_file
+    load_from_json_file = \
+    __import__("6-load_from_json_file").load_from_json_file
 
     args_list = []
 
