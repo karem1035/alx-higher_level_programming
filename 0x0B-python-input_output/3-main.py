8241e4c4c6b5b92210f16a2931f31adc0481da96
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-17 03:02:06 +0300

    task 3 redo

--- 0x0B-python-input_output/3-main.py
+++ 0x0B-python-input_output/3-main.py
@@ -6,10 +6,10 @@ s_my_list = to_json_string(my_list)
 print(s_my_list)
 print(type(s_my_list))
 
-my_dict = { 
+my_dict = {
     'id': 12,
     'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
+    'places': ["San Francisco", "Tokyo"],
     'is_active': True,
     'info': {
         'age': 36,
@@ -20,10 +20,10 @@ s_my_dict = to_json_string(my_dict)
 print(s_my_dict)
 print(type(s_my_dict))
 
-try:
-    my_set = { 132, 3 }
-    s_my_set = to_json_string(my_set)
-    print(s_my_set)
-    print(type(s_my_set))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     my_set = {132, 3}
+#     s_my_set = to_json_string(my_set)
+#     print(s_my_set)
+#     print(type(s_my_set))
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+'''a function that returns the JSON representation of an object (string):'''
 
 import json
 
+
 def to_json_string(my_obj):
-    
+    """Dumps a dic to json strn
+
+    Args:
+        my_obj (dict): dumps it a json string
+
+    Returns:
+        a_string: the string result
+    """
+    a_string = json.dumps(my_obj)
+    return a_string
