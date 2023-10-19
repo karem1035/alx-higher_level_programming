0b32aacebd4ec4cccaa830a77c63bb2b30a4d0f3
karem1035 <karemdarwesh1035@gmail.com>
2023-10-18 17:05:37 -0700

    task 4 redo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
--- Create table in the current database
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- Create a table
+CREATE TABLE first_table (
+	id int,
+	name VARCHAR(256)
+);
 
