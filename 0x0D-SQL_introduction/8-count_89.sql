0cb145165cb50548f10c1e7f9ee3145745c98269
karem1035 <karemdarwesh1035@gmail.com>
2023-10-18 17:40:33 -0700

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
--- script that displays the number of records with id = 89 in the table first_table of the database 
-SELECT COUNT(id) FROM first_table WHERE id=89;
+-- Displays the number of records with id = 89 in the table first_table
+SELECT COUNT(id) FROM first_table WHERE id = 89;
+
