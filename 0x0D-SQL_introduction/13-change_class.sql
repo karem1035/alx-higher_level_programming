a21d492bc25172f75cba7c9da6c814401dab9a89
karem1035 <karemdarwesh1035@gmail.com>
2023-10-19 17:47:16 -0700

    task 13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,4 @@
--- a script that removes all records with a score <= 5 in the table second_table
-DELETE FROM second_table WHERE score <= 5;
+-- Delete where score <= 5
+DELETE FROM second_table
+WHERE score <= 5;
+
