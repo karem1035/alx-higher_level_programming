9ede642fb93eec0af568eee50404b03da94ba12e
Karem <karemdarwesh1035@gmail.com>
2023-11-16 21:02:19 +0200

    task fix 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Converts the argument to integer if possible
 
-const intValue = parseInt(process.argv[2]);
+const intValue = process.argv[2];
 
 if (!isNaN(intValue)) {
   console.log('My number: ' + intValue);
