9a1a66642ea4e7278e1cd9610cf2133ca7e379b6
Karem <karemdarwesh1035@gmail.com>
2023-11-16 19:06:14 +0200

    task 1 fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Loop on array
 
-const lines = ['C is fun','Python is cool','JavaScript is amazing'] 
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const line of lines) {
   console.log(line);
 }
