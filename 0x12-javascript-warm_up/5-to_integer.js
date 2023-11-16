314564ccf8f5350519c5bae69d195f5a375ab7a4
Karem <karemdarwesh1035@gmail.com>
2023-11-16 20:49:13 +0200

    task 5 fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@
 const intValue = parseInt(process.argv[2]);
 
 if (!isNaN(intValue)) {
-  console.log("Number: " + intValue);
+  console.log("My number: " + intValue);
 } else {
   console.log("Not a number");
 }
