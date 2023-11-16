02cb02aaf04dbcf3f1b21441df6b8d0c72130c1d
Karem <karemdarwesh1035@gmail.com>
2023-11-16 08:09:47 +0200

    task 2 update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log('Argument found');
+// Checking the arugments
+
+const args = process.argv; 
+if (args.length === 2) {
+  console.log("Argument found");
+} else if (args.length > 2) {
+  console.log("Arguments found");
 } else {
-  console.log('No argument');
+  console.log("No argument");
 }
