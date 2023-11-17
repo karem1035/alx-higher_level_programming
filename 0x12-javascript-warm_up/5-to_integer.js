0d489be6c6d79e792ce1fe497d128ede27504b7f
Karem <karemdarwesh1035@gmail.com>
2023-11-17 21:04:48 +0200

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 // Checks the first arguments
 
-if (isNaN(process.argv[2])) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(process.argv[2]));
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
