39fbeacea34d2a73a64ca77d6525e66448982f9d
karem darwesh <karemdarwesh1035@gmail.com>
2024-02-16 16:08:03 -0800

    task 16 last update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
 -- A  script that lists all records of the table
-SELECT score, name FROM second_table WHERE SCORE IS NOT NULL ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
 
