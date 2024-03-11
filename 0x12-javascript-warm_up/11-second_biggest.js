17400369d4fe5adc882ca0f2f26c408196dc15bd
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-11 11:37:29 +0200

    task 11 redo

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-// a script that searches the second biggest integer in the list of arguments.
-if (process.argv.length <= 3) console.log(0);
-else {
-  const arr = process.argv.sort().reverse();
-  console.log(arr[1]);
+// searches the second biggest integer in the list of arguments.
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
