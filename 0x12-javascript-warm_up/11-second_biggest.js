ea5c9adb4bbb04cf07d0ac3b49bbd77cabf5f5cd
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-11 11:45:15 +0200

    task 11 finished

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // a script that searches the second biggest integer in the list of arguments.
-const arr = process.argv.slice(2).map((el) => Number(el));
+const arr = process.argv.slice(2).map((el) => parseInt(el));
 if (arr.length <= 1) console.log(0);
-else console.log(arr.sort().reverse()[1]);
+else console.log(arr.sort((a, b) => b - a)[1]);
