b30b7b340993648738699b73ca6e06a882087b4f
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-11 11:36:38 +0200

    small fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 // a script that searches the second biggest integer in the list of arguments.
 if (process.argv.length <= 3) console.log(0);
 else {
-  const arr = process.argv.slice(3);
-  console.log(arr.sort().reverse()[1]);
+  const arr = process.argv.sort().reverse();
+  console.log(arr[1]);
 }
