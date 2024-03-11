75cbbed1df5d344f7cd330ac2f66b7923aee9d01
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-11 11:03:50 +0200

    general fixations

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Logging My number: <first argument converted in integer> if the first argument can be converted
 
-if (Number(process.argv[2]) === 'NaN') console.log('suck me of');
+if (!parseInt(process.argv[2])) console.log('Not a number');
+else console.log(parseInt(process.argv[2]));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Printing using loops
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (str of arr) console.log(str);
+for (const str of arr) console.log(str);
