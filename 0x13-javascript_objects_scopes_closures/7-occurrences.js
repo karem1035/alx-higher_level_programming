30864b34bed58fb70c832157a431123aeb7ea466
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-12 17:12:11 +0200

    task 7 shortned

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
+// returns the number of occurrences in a list:
 exports.nbOccurences = function (list, searchElement) {
-  let n = 0;
-  for (const el of list) {
-    if (el === searchElement) n += 1;
-  }
-  return n;
+  return list.reduce((n, el) => (el === searchElement ? (n += 1) : n), 0);
 };
