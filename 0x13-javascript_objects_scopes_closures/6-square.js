e708fdf1c14871fc2ab7afdfe804f79db6803bc0
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-12 16:43:52 +0200

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Adding charPrint(c) method
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+};
