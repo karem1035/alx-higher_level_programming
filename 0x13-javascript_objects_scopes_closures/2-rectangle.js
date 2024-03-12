77251b0eda52cd6faccd2bec8301f8f8951f7a50
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-12 16:15:23 +0200

    Task 2 updated

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // updating the Rectangle class
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w && h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
