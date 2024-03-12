62d58187bfa988d8f9ca0700ae9a0b513b73d6f8
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-12 17:44:34 +0200

    Task 100 renamed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // mports an array and computes a new array.
-const list = require('./100-main').list;
+const list = require('./100-data').list;
 console.log(list);
 console.log(list.map((el, i) => el * i));
