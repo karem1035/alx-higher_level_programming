5b91fd3908a3c462e1bd80506a1c0d15ca3c2ac8
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-16 16:11:21 -0700

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values matches the argument. """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[2]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    namearg = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM states\
+                    WHERE name LIKE '{namearg}'\
+                    ORDER BY id ASC")
+
+    data = cursor.fetchall()
+    for i in data:
+        print(i)
+
+    cursor.close()
+    conn.close()
