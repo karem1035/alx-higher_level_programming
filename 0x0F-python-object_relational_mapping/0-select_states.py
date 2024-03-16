ebd96e5f135c95b2ab2f8989b15cbbeb5e64949c
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-16 15:44:17 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+#  a script that lists all states from the database hbtn_0e_0_usa:
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+	username = sys.argv[1]
+	password = sys.argv[2]
+	database = sys.argv[3]
+	
+	conn = MySQLdb.connect(
+		user=username,
+		passwd=password,
+		db=database
+		)
+
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM states")
+	for i in cursor:
+		print(i)
