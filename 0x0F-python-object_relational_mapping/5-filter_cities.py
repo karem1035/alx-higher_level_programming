52b59665ed21a9ca3f8416dc20275f5f221c0f3a
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-17 03:55:45 -0700

    Task 5 init

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values matches the argument. """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Establishing connection
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+
+    # Creating a cursor
+    cursor = conn.cursor()
+
+    # Executing the query
+    cursor.execute(f"SELECT cities.name FROM cities\
+                    INNER JOIN states\
+                    ON cities.state_id = states.id\
+                    WHERE states.name = '{argv[4]}'")
+
+    # Fetching and printing the retrieved data
+    data = cursor.fetchall()
+    output = []
+    for i in data:
+        output.append(i)
+    print(output)
+
+    # Cleaning the process
+    cursor.close()
+    conn.close()
