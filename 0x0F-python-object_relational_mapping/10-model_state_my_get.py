8560e52d12ff31eccc9e2266adf951b5929dadf9
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-18 07:36:03 -0700

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""Script that prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""
+a script that prints the State object with the name passed as argument
+"""
 from model_state import Base, State
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+if __name__ == '__main__':
+    # Constructing the connection URL using command-line arguments
+    url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+    # Creating the SQLAlchemy engine with pool pre-ping enabled
+    engine = create_engine(url, pool_pre_ping=True)
+
+    # Creating the tables defined in the models
     Base.metadata.create_all(engine)
+
+    # Creating a session factory bound to the engine
     Session = sessionmaker(bind=engine)
+
+    # Creating a new session
     session = Session()
-    state = session.query(State).filter_by(name=sys.argv[4]).first()
-    if state:
-        print("{}".format(state.id))
+
+    # Querying all State objects, ordered by id in ascending order
+    result = session.query(State).filter(State.name == argv[4]).first()
+
+    # Printing the result
+    if result:
+        print(result.id)
     else:
         print("Not found")
+
+    # Cleaning the session
     session.close()
