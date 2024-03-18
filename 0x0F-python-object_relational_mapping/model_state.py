870a4c6b45b7886be627c6ab59854865c3870c95
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-18 04:18:25 -0700

    task 6 update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class State"""
-
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
-
 class State(Base):
-    """ Class that defines properties of State"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = "user_account",
+    id = Column(Integer,  nullable=False, autoincrement=True, primary_key=True),
     name = Column(String(128), nullable=False)
