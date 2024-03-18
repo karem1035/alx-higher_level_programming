6ca2493dad16a5a84c000762faaac3db80d6156f
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-18 04:24:48 -0700

    Task 6 fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+
+"""
+This Python script defines a SQLAlchemy mapped class for a State entity.
+"""
+
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
+
 class State(Base):
-    __tablename__ = "user_account",
-    id = Column(Integer,  nullable=False, autoincrement=True, primary_key=True),
+    """"
+    Represesnts a state entity in the database.
+
+    Attributes:
+        id (int): The primary key of the state, auto-incremented.
+        name (str): The name of the state, max length 128 characters.
+    """
+    __tablename__ = "user_account"
+
+    id = Column(Integer,  nullable=False, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
