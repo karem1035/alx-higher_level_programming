df9c63b29c5a899cc08d3ff161244f0cb75c82ad
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-29 01:03:45 +0200

    Task chmod added

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
     if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+        print(f"Error code: {req.status_code}")
     else:
         print(req.text)
