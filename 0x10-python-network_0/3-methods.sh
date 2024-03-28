bf11688da0d58797d884dd5f9ccaf5f6f5d62f7a
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-27 22:37:59 -0700

    -i added to grep

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # can take URL and displays all HTTP methods the server will accept in
-curl -sI "$1" | grep "Allow: " | cut -d' ' -f 2-
+curl -sI "$1" | grep -i "Allow: " | cut -d' ' -f 2-
