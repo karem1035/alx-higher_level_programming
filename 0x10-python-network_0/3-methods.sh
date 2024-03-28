9384af181bfe7db27bbb14448987f99f968821d9
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-27 22:36:22 -0700

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays all HTTP methods the server will accept.
+# can take URL and displays all HTTP methods the server will accept in
 curl -sI "$1" | grep "Allow: " | cut -d' ' -f 2-
