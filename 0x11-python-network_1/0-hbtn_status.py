c6b387713351b6772f2d471eb9c28d37da84e09e
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 19:09:28 +0200

    task main name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-print("Body response:")
-print(f"\t - type: {type(body)}")
-print(f"\t - content: {body}")
-print(f"\t - utf8 content: {body.decode('utf-8')}")
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+    print("Body response:")
+    print(f"\t - type: {type(body)}")
+    print(f"\t - content: {body}")
+    print(f"\t - utf8 content: {body.decode('utf-8')}")
