d64466fd99125d46a9c7bd42b35a8e73b8fbfcca
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-27 17:34:40 -0700

    Task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-response=$(curl -s "$1")
-response_size=$(echo -n "$response" | wc -c)
-echo "$response_size"
+response=$(curl -s -I "$1" | grep -i Content-Length:)
+size=$(echo "$response" | cut -d ':' -f2 | tr -d ' ')
+echo "$size"
