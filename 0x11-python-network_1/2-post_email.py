d7ca02625d2e549854ff77e19f0999fc97c66fc0
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 20:55:37 +0200

    task line down

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,8 +7,8 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    req = urllib.request.Request(url, data=data, method='POST')
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(req) as response:
         response_content = response.read().decode('utf-8')
         print(response_content)
