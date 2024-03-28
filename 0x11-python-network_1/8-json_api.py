6f1663dab0bae6561908ca7fe89ce903ff453175
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-29 01:55:58 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" takes in a letter and sends it as a POST request to URL"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    data = {"q": letter}
+    url = 'http://0.0.0.0:5000/search_user'
+
+    req = requests.post(url, data=data)
+    try:
+        r = req.json()
+        if r == {}:
+            print("No result")
+        else:
+            print(f"[{r.id}] {r.name}")
+    except ValueError:
+        print("Not a valid JSON")
