b2d85fe51f43d7f7689b8980bbdd07c30db5a038
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 20:52:59 +0200

    Task 2 line shifted down

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,11 +4,11 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
 if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    req = urllib.request.Request(url, data=data, method='POST')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     with urllib.request.urlopen(req) as response:
         response_content = response.read().decode('utf-8')
         print(response_content)
