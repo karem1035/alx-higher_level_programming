8567db4b0fa7457bad400ada907ae1bae3d5c6c9
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 19:13:36 +0200

    Task 1 update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,8 @@
 import sys
 import urllib.request
 
-url = sys.argv[1]
-response = urllib.request.urlopen(url)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = urllib.request.urlopen(url)
 
-print(response.headers.get('X-Request-Id'))
+    print(response.headers.get('X-Request-Id'))
