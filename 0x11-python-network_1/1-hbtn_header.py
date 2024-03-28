db66810e30c98c650f1bf9efa651954d1cce6f96
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 19:17:08 +0200

    task 1 chmod

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,9 @@
 """ prints X-Request-Id variable found in the header. """
 import sys
 import urllib.request
+
 if __name__ == "__main__":
     url = sys.argv[1]
     response = urllib.request.urlopen(url)
-    print(dict(response.headers).get('X-Request-Id'))
+
+    print(response.headers.get('X-Request-Id'))
