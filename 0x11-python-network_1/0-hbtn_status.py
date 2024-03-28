d9272e8ae3c01701b0f03cba9f866c8b305fba51
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 19:10:50 +0200

    space removed before the tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
-    print(f"\t - type: {type(body)}")
-    print(f"\t - content: {body}")
-    print(f"\t - utf8 content: {body.decode('utf-8')}")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
