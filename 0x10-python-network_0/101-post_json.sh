702962dd4ffaeee271bb8d2402ed48c780334b35
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-27 19:43:37 -0700

    chmod

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+# a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
