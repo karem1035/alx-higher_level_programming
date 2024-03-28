efc19d8a3a77a6ffa4b86bc94d014251bc4ad633
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 19:21:42 +0200

    Task 1 chmod

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,5 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = urllib.request.urlopen(url)
-
-    print(response.headers.get('X-Request-Id'))
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
