1688ac0172e82a922e22578d1e79e92d045fc513
karem darwesh <karemdarwesh1035@gmail.com>
2024-03-28 20:46:13 +0200

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,11 +2,12 @@
 """ send POST request to the URL with the email, displays the body """
 import sys
 import urllib.request
-
+import urllib.parse
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    req = urllib.request.Request(url, email=email, method='POST')
     with urllib.request.urlopen(req) as response:
         response_content = response.read().decode('utf-8')
         print(response_content)
