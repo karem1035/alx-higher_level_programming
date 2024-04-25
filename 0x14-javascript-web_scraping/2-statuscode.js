1d1c3193292d862c66d90750f5302205fc96aca0
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-25 21:50:29 +0200

    error handeled

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,9 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log(`code: ${response.statusCode}`);
 });
