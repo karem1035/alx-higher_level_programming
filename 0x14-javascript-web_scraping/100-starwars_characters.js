2f095c2a65c2d038d906bd3671d663f33943b85c
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-26 14:10:22 +0300

    task adv

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const movieID = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films';
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 request(url, function (_err, __res, body) {
-  const charArray = JSON.parse(body).results[movieID].characters;
-  for (let i = 0; i < charArray.length; i++) {
-    request(charArray[i], function (_err, __res, body) {
+  const chars = JSON.parse(body).characters;
+
+  for (let i = 0; i < chars.length; i++) {
+    request(chars[i], function (_err, __res, body) {
       console.log(JSON.parse(body).name);
     });
   }
